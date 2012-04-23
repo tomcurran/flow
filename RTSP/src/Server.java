f926d7cb08f29394b7fd8e6a5559272e8c08bf32
Tom Curran <tomcurran5@gmail.com>
2012-04-23 18:03:30 +0100

    moved videostream into server model

--- RTSP/src/Server.java
+++ RTSP/src/Server.java
@@ -18,9 +18,8 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.Timer;
 
-
+import server.model.VideoStream;
 import shared.RTPpacket;
-import shared.VideoStream;
 
 @SuppressWarnings("serial")
 public class Server extends JFrame implements ActionListener {
