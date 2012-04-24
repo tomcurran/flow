8d7323af0958304334baa7826f4676167e82a115
Andrew White <scurious.aw@gmail.com>
2012-04-24 11:13:56 +0100

    GUI Development - Added icons classes for the buttons

---
+++ RTSP/src/client/icons/PauseIcon.java
@@ -0,0 +1,43 @@
+package client.icons;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.RenderingHints;
+
+import javax.swing.Icon;
+
+public class PauseIcon implements Icon {
+
+	@Override
+	public int getIconHeight() {
+		return 5;
+	}
+
+	@Override
+	public int getIconWidth() {
+		return 25;
+	}
+
+	@Override
+	public void paintIcon(Component c, Graphics g, int x, int y) {
+		//Points for the rectangles that makes up the pause button. 
+		Point p1 = new Point(5, 5);
+        Point p2 = new Point(20, 5);
+       
+		
+        //Turns on antialiasing, making the appearance cleaner. 
+		((Graphics2D) g).setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                 RenderingHints.VALUE_ANTIALIAS_ON);
+		
+		//constructs two rectangles for a pause button.
+        g.setColor(Color.black);
+        g.fillRect(p1.x, p1.y, getIconWidth(), getIconHeight());
+        g.fillRect(p2.x, p2.y, getIconWidth(), getIconHeight());
+        
+	}
+
+}
+
